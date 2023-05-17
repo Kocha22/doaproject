f99b5787c3eb2e0e46bac186d221dd6d99cb1e4f
Zhumabekov Kylychbek <jailoosonkul@gmail.com>
2023-05-17 19:56:16 +0600

    multiple selects options

--- src/views/account/ListBiofarm.vue
+++ src/views/account/ListBiofarm.vue
@@ -1,6 +1,41 @@
 <template>
     <div v-if="loading">Loading...</div>
     <div class="content-wrapper">
+        <select id='oblast' name="oblast" class="choice" @change.prevent="onChange($event)" v-model="selectedOption">
+                <option value="0">Выберите адрес</option>
+                <option
+                    v-for="(post, i) in posts2"
+                    :value="post.id"
+                    :key="post.id"  
+                    :data-id="post.id"                              
+                    >{{ post.name_ru }}</option
+                >
+                </select>
+                <select id="rayon" name="rayon"  class="choice"  @change.prevent="onChange($event)" v-model="selectedRayon">
+                <option value='0'>-- Выберите из списка  --</option>
+                
+                <option v-show="rayons.length"
+                    v-for="(post, i) in rayons"
+                    :value="post.id"
+                    :key="post.id"  
+                    :data-id="post.id"                                
+                    >{{ post.name_ru }}</option
+                >
+                </select>
+                <select id="village" name="village"  class="choice" v-model="selectedVillage">
+                <option value='0'>-- Выберите из списка  --</option>
+                <option v-show="village.length"
+                    v-for="(post, i) in village"
+                    :value="post.id"
+                    :key="post.id"  
+                    :data-id="post.id"                                  
+                    >{{ post.name_ru }}</option
+                >
+        </select>
+
+        
+        <input type="text" v-model="searchQuery" placeholder="Search">
+        
         <table id="customers">        
             <thead>
                 <tr>
@@ -15,7 +50,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="post  in posts" :key="post.id">
+                <tr v-for="post  in filteredData" :key="post.id">
                 <td>{{ post.id }}</td>
                 <td>{{ post.farmer_status }}</td>
                 <td>
@@ -73,24 +108,117 @@
 import VPagination from "@hennge/vue3-pagination";
 import "@hennge/vue3-pagination/dist/vue3-pagination.css";
 import axios from 'axios'
-import { onBeforeMount, ref } from 'vue';
+import { onBeforeMount, ref, computed } from 'vue';
 
-    let page = ref(1)
-    let posts = ref(null)
-    let pageCount = ref(null)
+let loading = ref(false)
+let id = ref(null)
+let posts2 = ref(null)
+let rayons = ref(null)
+let village = ref(null)
+let page = ref(1)
+let posts = ref(null)
+let pageCount = ref(null)
+let selectedOption = ref('')
+let selectedRayon = ref('')
+let selectedVillage = ref('')
+let searchQuery = ref('')
+let active = ref(false);
 
-    onBeforeMount(async () => {
-        await getPosts()
-    })
+onBeforeMount(async () => {
+    await getPosts()
+})
 
-    const getPosts = async () => {
-        try {
-            const response = await axios.get('api/biofarms?page=' + page.value)
-            pageCount.value = response.data.page_count
-            posts.value = response.data.paginate.data
-            console.log(posts)
-        } catch (err) {
-            console.log(err)
-        }
+const getPosts = async () => {
+    try {
+        const response = await axios.get('api/biofarms?page=' + page.value)
+        const areas = await axios.get('api/area')
+        posts2.value = areas.data[0].children
+        pageCount.value = response.data.page_count
+        posts.value = response.data.paginate.data
+        console.log(posts)
+        loading = true
+    } catch (err) {
+        console.log(err)
     }
+}
+
+const getArea = async (id) => {
+    try {
+        const rayonData = await axios.get(`api/getarea/${id}`)
+        rayons.value = rayonData.data.children;
+        active = true
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const getVillage = async (id) => {
+    try {
+        const villageData = await axios.get(`api/getarea/${id}`)
+        village.value = villageData.data.children;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+function onChange (event) {
+    let area_id = event.target.getAttribute('id');
+    console.log(area_id);
+    id = event.target.options[event.target.options.selectedIndex].getAttribute('data-id');
+    if(area_id === 'oblast') {
+        getArea(id)
+        selectedRayon.value = '';
+    } else if(area_id === 'rayon') {
+        getVillage(id)
+        selectedVillage.value = ''
+    }
+    
+}
+
+const filteredData = computed(() => {
+      let filtered = posts.value;
+      console.log(filtered);
+      
+      // Filter based on selected option
+      if (selectedOption.value) {
+        filtered = filtered.filter(function(item) {
+            if(selectedOption.value == 0) {
+                return item;
+            } else if (item.oblast == selectedOption.value) {
+                return item;
+            }                 
+        });
+         console.log("Filtered by Option:", filtered);
+      } 
+
+      if (selectedRayon.value) {
+            filtered = filtered.filter(function(item) {
+                if(selectedRayon.value == 0) {
+                    return item;
+                } else if (item.rayon == selectedRayon.value) {
+                    return item;
+                }   
+            });
+      }
+
+      if (selectedVillage.value) {
+            filtered = filtered.filter(function(item) {
+                if(selectedVillage.value == 0) {
+                    return item;
+                } else if (item.village == selectedVillage.value) {
+                    return item;
+                }   
+            });
+      }
+      
+      // Filter based on search query
+      if (searchQuery.value) {
+        const query = searchQuery.value.toLowerCase();
+        filtered = filtered.filter(item => {
+          return item.farmer_name.toLowerCase().includes(query);
+        });
+      }
+      
+      return filtered;
+    });
 </script>
